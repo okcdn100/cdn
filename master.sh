04d0f6c6afc89670eb6b59f9b298c080a0af1a8b
lambq <lisongxia@vip.qq.com>
2025-03-16 23:35:39 +0800

    lambq update

--- master.sh
+++ master.sh
@@ -220,7 +220,7 @@ else
 fi
 
 cd /opt/
-download "https://github.com/LoveesYe/cdnflydadao/raw/main/master/$tar_gz_name" "https://github.com/LoveesYe/cdnflydadao/raw/main/master/$tar_gz_name" "$tar_gz_name"
+download "https://github.com/okcdn100/cdn/raw/main/master/$tar_gz_name" "https://github.com/okcdn100/cdn/raw/main/master/$tar_gz_name" "$tar_gz_name"
 
 tar xf $tar_gz_name
 rm -rf cdnfly
